d9ff592a175ad29159b622cb818c5dd2fc879734
Memo <fretn655@gmail.com>
2026-07-20 18:36:59 -0600

    se crean df de los dos datasets con la libreria pandas

---
+++ SC51_Reto_TorresLuis.py
@@ -0,0 +1,10 @@
+# -------------- importaciones
+
+import pandas as pd
+ 
+# -------------- fin de las importaciones
+
+# se cargan los dos csv del reto de autotransporte federal de turismo
+df_entidades = pd.read_csv(r"C:\Users\sasor\Desktop\Tec de mty\5. Analisis de datos\1. Visualizacion de analisis descriptivo\Proyecto\reto_3.csv", encoding="latin-1")
+df_meses = pd.read_csv(r"C:\Users\sasor\Desktop\Tec de mty\5. Analisis de datos\1. Visualizacion de analisis descriptivo\Proyecto\reto_1.csv", encoding="latin-1")
+
